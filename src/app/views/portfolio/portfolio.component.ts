a3a506d8f12dfe96a47f98e3c7d36495b0e26aec
Mizanur Rahman <mizan.prog@gmail.com>
2022-11-06 17:23:05 +0600

    update4

--- src/app/views/portfolio/portfolio.component.ts
+++ src/app/views/portfolio/portfolio.component.ts
@@ -15,6 +15,7 @@ export class PortfolioComponent implements OnInit {
     { name:'Erp Management',subtitle:'multiple user role based ' ,catagory:'web' ,image:'../../../assets/img/project/web4.png', language:['Laravel8', 'Jquery','Bootstrap', 'Html' , 'Css' ] , git: "https://github.com/mizan8102/InventoryJQueryLaravel"},
     { name:'News App',subtitle:`It's only frontend project` ,catagory:'web' ,image:'../../../assets/img/project/work3.png', language:['Angular Js','Material Ui','Html','Css' ] , git: "https://github.com/mizan8102/angular-news-app"},
     { name:'Book Store',subtitle:`It's only frontend project` ,catagory:'web' ,image:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png', language:['VueJs','Firebase','Html','Css' ] , git: "https://github.com/mizan8102/bookstore"},
+    { name:'shop website',subtitle:`this shop website` ,catagory:'web' ,image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsmLG8r6Dw7tVx2C0e43asjyx9knai4dC7WA&usqp=CAU', language:['Laravel 8','VueJs','TailwindCss','Html','Css' ] , git: "https://github.com/mizan8102/e-commerce"},
 
     // android project
 
@@ -29,12 +30,17 @@ export class PortfolioComponent implements OnInit {
     { name:'World Time App',subtitle:'help know any location time' ,catagory:'android' ,image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVns0nK-M_3nU-FDr1F0N-_HqsUupY2CV5nQ&usqp=CAU', language:['Flutter', 'Dart','Api'] , git: "https://github.com/mizan8102/worldtime"},
 
 
+    // desktop application
 
-    // { name:'School Management', catagory:'web' ,image:'../../../assets/img/project/work2.png', language:'Laravel & VueJs' , git: "https://github.com/mizan8102/ever-build-corporation"},
-    // { name:'Learning App', catagory:'android' ,image:'../../../assets/img/project/work3.png', language:'Anroid Studio (Java)' , git: "https://github.com/mizan8102/ever-build-corporation"},
-    // { name:'Apir Virtual AI ' ,image:'../../../assets/img/project/work4.jpg', language:'Anroid Studio (Java)' , git: "https://github.com/mizan8102/ever-build-corporation"},
-    // { name:'Hospital Management' ,image:'../../../assets/img/project/work5.png', language:'Java Swing' , git: "https://github.com/mizan8102/ever-build-corporation"},
-    // { name:'News ' ,image:'../../../assets/img/project/work1.png', language:'Angular' , git: "https://github.com/mizan8102/ever-build-corporation"},
+    { name:'Hospital Management System',subtitle:'this application include pharmecy,hospital all data & report etc' ,catagory:'desktop' ,image:'../../../assets/img/project/desktop1.png', language:['Java','Java Swing', 'MySQL'] , git: "https://github.com/mizan8102/Hospital_Management"},
+    { name:'Hotel Management System',subtitle:'all hostel data & report etc inclue here' ,catagory:'desktop' ,image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDny5Lbh5ivS4NPEgwFoHjdxyYHbr1dwd_2A&usqp=CAU', language:['Java','Java Swing', 'MySQL'] , git: "https://github.com/mizan8102/JavaSwing-HotelMS"},
+    { name:'Inventory Management System',subtitle:'this is inventory management' ,catagory:'desktop' ,image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDny5Lbh5ivS4NPEgwFoHjdxyYHbr1dwd_2A&usqp=CAU', language:['Java','Java Swing', 'MySQL'] , git: "https://github.com/mizan8102/rmc_management"},
+
+    //others project
+
+    { name:'Snake Games ',subtitle:'this is simple snake games' ,catagory:'others' ,image:'../../../assets/img/project/others1.png', language:['C Language'] , git: "https://github.com/mizan8102/snake-games"},
+
+
   ]
   constructor() { }
 
